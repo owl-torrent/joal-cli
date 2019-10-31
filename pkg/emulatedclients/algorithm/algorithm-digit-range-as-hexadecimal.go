ef4fefaa131199d4ab4e19b298024d08444b7b8f
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2019-10-31 22:30:06 +0100

    Fix algorithm-digit-range-as-hexadecimal definition

--- pkg/emulatedclients/algorithm/algorithm-digit-range-as-hexadecimal.go
+++ pkg/emulatedclients/algorithm/algorithm-digit-range-as-hexadecimal.go
@@ -5,23 +5,25 @@ import (
 	"github.com/anthonyraymond/joal-cli/pkg/randutils"
 	"github.com/pkg/errors"
 	"strconv"
+	"strings"
 )
 
 type NumRangeAsHexadecimalAlgorithm struct {
 	Min               *int64 `yaml:"min"`
 	Max               *int64 `yaml:"max"`
-	LeftPadWithZeroes bool   `yaml:"leftPadWithZeroes"`
+	TrimLeadingZeroes bool   `yaml:"trimLeadingZeroes"`
 	MaxLength         *int   `yaml:"maxLength"`
 	Case              *Case  `yaml:"case"`
 }
 
 func (a *NumRangeAsHexadecimalAlgorithm) Generate() string {
 	hex := strconv.FormatInt(randutils.Range(*a.Min, *a.Max), 16)
-	if a.LeftPadWithZeroes {
-		hex = fmt.Sprintf("%0"+strconv.Itoa(*a.MaxLength)+"s", hex) // left pad with zeros
-	}
+	hex = fmt.Sprintf("%0"+strconv.Itoa(*a.MaxLength)+"s", hex) // left pad with zeros
 	if len(hex) > *a.MaxLength {
 		hex = hex[len(hex)-*a.MaxLength:] // substring to keep only the 8 rightmost characters
+	}
+	if a.TrimLeadingZeroes {
+		hex = strings.TrimLeft(hex, "0")
 	}
 	return a.Case.ApplyCase(hex)
 }
--- pkg/emulatedclients/algorithm/algorithm-digit-range-as-hexadecimal_test.go
+++ pkg/emulatedclients/algorithm/algorithm-digit-range-as-hexadecimal_test.go
@@ -8,7 +8,7 @@ func TestDigitRangeAsHexadecimalAlgorithm_Generate(t *testing.T) {
 	type fields struct {
 		Min               *int64
 		Max               *int64
-		LeftPadWithZeroes bool
+		TrimLeadingZeroes bool
 		maxLength         *int
 		Case              Case
 	}
@@ -17,19 +17,19 @@ func TestDigitRangeAsHexadecimalAlgorithm_Generate(t *testing.T) {
 		fields fields
 		want   string
 	}{
-		{name: "shouldFormatWithLeadingZeroes", fields: fields{Min: i64p(1), Max: i64p(1), LeftPadWithZeroes: true, maxLength: i8p(8)}, want: "00000001"},
-		{name: "shouldFormatWithoutLeadingZeroes", fields: fields{Min: i64p(1), Max: i64p(1), LeftPadWithZeroes: false, maxLength: i8p(8)}, want: "1"},
-		{name: "shouldFormatLargeNumbersAndTrimAccordingToLength", fields: fields{Min: i64p(9223372036854775807), Max: i64p(9223372036854775807), LeftPadWithZeroes: true, maxLength: i8p(9)}, want: "fffffffff"},
-		{name: "shouldFormatLargeNumbersAndTrimAccordingToLength2", fields: fields{Min: i64p(9223372036854775807), Max: i64p(9223372036854775807), LeftPadWithZeroes: false, maxLength: i8p(9)}, want: "fffffffff"},
-		{name: "shouldApplyCase", fields: fields{Min: i64p(12), Max: i64p(12), LeftPadWithZeroes: false, maxLength: i8p(9), Case: Lower}, want: "c"},
-		{name: "shouldApplyCase", fields: fields{Min: i64p(12), Max: i64p(12), LeftPadWithZeroes: false, maxLength: i8p(9), Case: Upper}, want: "C"},
+		{name: "shouldFormatWithLeadingZeroes", fields: fields{Min: i64p(1), Max: i64p(1), TrimLeadingZeroes: false, maxLength: i8p(8)}, want: "00000001"},
+		{name: "shouldFormatWithoutLeadingZeroes", fields: fields{Min: i64p(1), Max: i64p(1), TrimLeadingZeroes: true, maxLength: i8p(8)}, want: "1"},
+		{name: "shouldFormatLargeNumbersAndTrimAccordingToLength", fields: fields{Min: i64p(9223372036854775807), Max: i64p(9223372036854775807), TrimLeadingZeroes: true, maxLength: i8p(9)}, want: "fffffffff"},
+		{name: "shouldFormatLargeNumbersAndTrimAccordingToLength2", fields: fields{Min: i64p(9223372036854775807), Max: i64p(9223372036854775807), TrimLeadingZeroes: false, maxLength: i8p(9)}, want: "fffffffff"},
+		{name: "shouldApplyCase", fields: fields{Min: i64p(12), Max: i64p(12), TrimLeadingZeroes: false, maxLength: i8p(8), Case: Lower}, want: "0000000c"},
+		{name: "shouldApplyCase", fields: fields{Min: i64p(12), Max: i64p(12), TrimLeadingZeroes: false, maxLength: i8p(8), Case: Upper}, want: "0000000C"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			a := &NumRangeAsHexadecimalAlgorithm{
 				Min:               tt.fields.Min,
 				Max:               tt.fields.Max,
-				LeftPadWithZeroes: tt.fields.LeftPadWithZeroes,
+				TrimLeadingZeroes: tt.fields.TrimLeadingZeroes,
 				MaxLength:         tt.fields.maxLength,
 				Case:              &tt.fields.Case,
 			}
