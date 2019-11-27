192284f7a96229745596f8d2c9e18345546d824c
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2019-11-27 23:52:22 +0100

    Add TagNameFunction for validator to print correct yaml path

---
+++ pkg/validator/validator.go
@@ -0,0 +1,17 @@
+package validator
+
+import (
+	"reflect"
+	"strings"
+)
+
+var TagNameFunction = func(fld reflect.StructField) string {
+	name := strings.SplitN(fld.Tag.Get("yaml"), ",", 2)[0]
+	if name == "" {
+		name = strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+	}
+	if name == "-" {
+		return ""
+	}
+	return name
+}
---
+++ pkg/validator/validator_test.go
@@ -0,0 +1,27 @@
+package validator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func Test_TagNameFunction_ShouldMatchName(t *testing.T) {
+	s := struct {
+		JsonName  string `json:"naming"`
+		JsonName2 string `json:"naming,omitempty"`
+		YamlName  string `yaml:"naming"`
+		YamlName2 string `yaml:"naming,omitempty"`
+		Empty     string
+	}{}
+
+	assert.Equal(t, "naming", TagNameFunction(mustFieldByName(reflect.TypeOf(s).FieldByName("JsonName"))))
+	assert.Equal(t, "naming", TagNameFunction(mustFieldByName(reflect.TypeOf(s).FieldByName("JsonName2"))))
+	assert.Equal(t, "naming", TagNameFunction(mustFieldByName(reflect.TypeOf(s).FieldByName("YamlName"))))
+	assert.Equal(t, "naming", TagNameFunction(mustFieldByName(reflect.TypeOf(s).FieldByName("YamlName2"))))
+	assert.Equal(t, "", TagNameFunction(mustFieldByName(reflect.TypeOf(s).FieldByName("Empty"))))
+}
+
+func mustFieldByName(st reflect.StructField, b bool) reflect.StructField {
+	return st
+}
