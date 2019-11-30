e93ca4fa48e547813c5919ab54069bc9bc3a5e0d
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2019-11-30 21:54:44 +0100

    Add EmulatedClient test for unmarshaling

--- pkg/emulatedclients/emulated-client_test.go
+++ pkg/emulatedclients/emulated-client_test.go
@@ -1,6 +1,7 @@
 package emulatedclients
 
 import (
+	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v2"
 	"os"
 	"path/filepath"
@@ -20,4 +21,13 @@ func TestEmulatedClient_Unmarshall(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to validate EmulatedClient: %v", err)
 	}
+
+	assert.Equal(t, "qBittorrent", client.Name)
+	assert.Equal(t, "3.3.1", client.Version)
+	assert.Equal(t, int32(200), client.NumWant)
+	assert.Equal(t, int32(0), client.NumWantOnStop)
+	assert.NotNil(t, client.KeyGenerator)
+	assert.NotNil(t, client.PeerIdGenerator)
+	assert.NotNil(t, client.Announcer)
+	assert.NotNil(t, client.Listener)
 }
