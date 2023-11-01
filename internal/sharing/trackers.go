1f305ab881500402626a8f6ab564440072cb98df
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2023-11-01 19:23:23 +0100

    initiate Trackers todo list

---
+++ internal/sharing/trackers.go
@@ -0,0 +1,6 @@
+package sharing
+
+//TODO: Trackers
+// - for each tracker that require announce, gather announceRequests
+// - on announce success => promote tracker in tier
+// - on announce error => put back a tracker at the end of his
---
+++ internal/sharing/trackers_test.go
@@ -0,0 +1,4 @@
+package sharing
+
+type Trackers struct {
+}
